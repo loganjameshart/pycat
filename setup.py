50fb11abca5a3c4f59670807f9446b01c979864e
Logan James Hart <116290186+loganjameshart@users.noreply.github.com>
2023-06-21 11:44:14 -0500

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycat',
+version='0.1',
+description='simple pure-Python version of netcat',
+url='https://github.com/loganjameshart/pycat',
+author='Logan James Hart',
+author_email='logan@loganjameshart.com',
+license='MIT',
+packages=['pycat']
+)
