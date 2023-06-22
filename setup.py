352c706be4914f7f9847119f83af6aea29afdf11
Logan James Hart <116290186+loganjameshart@users.noreply.github.com>
2023-06-22 15:11:46 -0500

    Update setup.py
    added py_modules argument

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='pycat',
-version='0.1',
-description='simple pure-Python version of netcat',
-url='https://github.com/loganjameshart/pycat',
-author='Logan James Hart',
-author_email='logan@loganjameshart.com',
-license='MIT',
-packages=find_packages()
+setup(
+    name="pycat",
+    version="0.1",
+    description="makes boilerplate directory and files for Python projects",
+    url="https://github.com/loganjameshart/pytemplate",
+    author="Logan James Hart",
+    author_email="logan@loganjameshart.com",
+    license="MIT",
+    py_modules=["pycat"],
+    entry_points={
+        "console_scripts": [
+            "pycat = pycat:main",
+        ]
+    },
 )
